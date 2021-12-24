1cca812907fba68160b00e3bbdf921d15d6d123d
Enrique Khai <nigelotan00@gmail.com>
2021-12-24 21:25:23 +0800

    Cleaned up add_new_project.sh.

--- projects/add_new_project.sh
+++ projects/add_new_project.sh
@@ -7,12 +7,12 @@ source '../settings/libraries/languages.sh'
 num_lang=${#language[@]}
 
 
-# Enter project name.
+# Get project name.
 printf 'Project Name: '; read project_name
 printf '\n'
 
 
-# Enter implementation.
+# Get implementation.
 printf 'Implementation:\n'
 
 
@@ -24,11 +24,14 @@ do
     {
         printf '  %2d) %s\n' $i "${language[$i]}"
     }
-done; printf '\n'
+done
+
+printf '\n'
 
 
 # Get choice.
 printf 'Select an option: '; read idx
+printf '\n'
 
 
 # Retrieve corresponding language name and extension.
@@ -37,16 +40,16 @@ f_ext="${file_ext[$lang]}"
 
 
 # Update UI with choice.
-printf '\033[1A'
+printf '\033[2A'
 printf 'Select an option: %d (%s)\n\n' $idx $lang
 
 
 # If a directory with $project_name already exists,
 [ -d "$project_name" ] && \
 {
-    for (( i=1 ;; ++i ))
+    for (( i = 1 ;; i++ ))
     do
-        # try adding suffixes "_1", "_2", and so on.
+        # try adding suffixes '_1', '_2', and so on.
         [ -d "${project_name}_$i" ] || \
         {
             project_name="${project_name}_$i"
@@ -61,7 +64,7 @@ printf "> Creating directory for $project_name..."
 
 cp -r '../settings/templates/project' "$project_name"
 
-printf " done!\n"
+printf ' done!\n'
 
 
 # Create test harness for project.
@@ -75,14 +78,14 @@ printf "> Creating test harness for $project_name..."
 [ -f "$templates/$test_harness" ] && \
 {
     cp "$templates/$test_harness" "$project_name/$test_harness"
-    printf " done!\n\n"
+    printf ' done!\n\n'
 }
 
 
 # If no template harness is found, create an empty one.
 [ -f "$templates/$test_harness" ] || \
 {
-    printf "none found.\n\n"
+    printf ' none found.\n\n'
     touch "$project_name/$test_harness"
 }
 
